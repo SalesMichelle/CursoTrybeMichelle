6049d191eb8043aa44a23d03f1863728ff3e64f1
SalesMichelle <michelle.santos@mottu.com.br>
2025-04-20 19:14:37 -0300

    Exercício de números com While

---
+++ EstudoPessoal/LacosDeRepeticao/While.cs
@@ -0,0 +1,20 @@
+﻿// Imprimir o número até o que o usuário escolher
+
+using System ;
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine()); // A variável numero é de acordo com que o usuário digitar.
+
+            int i = 1 ; // Inicializa a variável i
+
+            while (i <= numero )
+            {
+                Console.Write(i + " ") ; // Escreve na tela os número de 1 até o "numero"
+                i++ ;
+            }
+        }
+    }
